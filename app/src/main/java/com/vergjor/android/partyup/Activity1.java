07d9fd521833611b7cfe95685237771ab31950f2
KaltrinI <kaltrinismaili@gmail.com>
2018-05-07 20:02:08 +0200

    Added Client files (Database not finished)

---
+++ app/src/main/java/com/vergjor/android/partyup/Activity1.java
@@ -0,0 +1,32 @@
+package com.vergjor.android.partyup;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        final Button btRegister = (Button) findViewById(R.id.btLogin);
+
+        final TextView registerLink = (TextView) findViewById(R.id.tvRegister);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(Activity1.this, RegisterActivity.class);
+                        Activity1.this.startActivity(registerIntent);
+            }
+        });
+    }
+}
