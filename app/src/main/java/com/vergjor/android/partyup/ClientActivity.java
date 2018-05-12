951e6ef68cff6de61f7e1ed7ad288b6cec19e12f
VerGjor <VerGjor@users.noreply.github.com>
2018-05-13 01:12:20 +0200

    gradel and verion errors

--- app/src/main/java/com/vergjor/android/partyup/ClientActivity.java
+++ app/src/main/java/com/vergjor/android/partyup/ClientActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
--- app/src/main/java/com/vergjor/android/partyup/RecyclerAdapter.java
+++ app/src/main/java/com/vergjor/android/partyup/RecyclerAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
 /**
  * Created by Veronika Gjoreva on 01/03/2018.
  */
