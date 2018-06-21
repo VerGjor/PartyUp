a6aafad1ecb9e5b7d953c01936dbee045c18619e
VerGjor <VerGjor@users.noreply.github.com>
2018-06-21 23:44:37 +0200

    Service added (Music) + List of reservations (partially done)

--- app/src/main/java/com/vergjor/android/partyup/AddEventActivity.java
+++ app/src/main/java/com/vergjor/android/partyup/AddEventActivity.java
@@ -2,8 +2,6 @@ package com.vergjor.android.partyup;
 
 import android.annotation.SuppressLint;
 import android.arch.persistence.room.Room;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +12,6 @@ import android.widget.EditText;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-import com.cloudinary.android.MediaManager;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -42,7 +39,6 @@ public class AddEventActivity extends AppCompatActivity {
         e_name = findViewById(R.id.txteventname);
         nr_R=findViewById(R.id.txtReservInt);
         subm=findViewById(R.id.btnSubmitAdd);
-        imagepick=findViewById(R.id.btnImagePick);
         eventTime = findViewById(R.id.txtTime);
         url = "url";
 
@@ -92,33 +88,6 @@ public class AddEventActivity extends AppCompatActivity {
         });
 
 
-        imagepick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(intent, RESULT_LOAD_IMG);
-                }
-
-            }
-
-
-            protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-                AddEventActivity.super.onActivityResult(requestCode,resultCode,data);
-
-
-                if (resultCode == RESULT_OK) {
-                    Uri fullPhotoUri = data.getData();
-
-                    url = MediaManager.get().upload(fullPhotoUri).dispatch();
-
-
-                }
-            }
-        });
     }
 }
 
